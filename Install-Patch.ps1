805bf9b183d0ccaf9132e7fc683d9f7242efa6a0
Raghu-Orea <80732797+Raghu-Orea@users.noreply.github.com>
2021-03-16 12:00:52 +0530

    Add files via upload

---
+++ Install-Patch.ps1
@@ -0,0 +1,23 @@
+﻿# Specify the location of the *.msu files
+$updatedir = "C:\Temp\Patch\"
+###
+
+$files = Get-ChildItem $updatedir -Recurse
+$msus = $files | ? {$_.extension -eq ".msu"}
+
+foreach ($msu in $msus)
+{
+    write-host "Installing update $msu ..."
+    $fullname = $msu.fullname
+    # Need to wrap in quotes as folder path may contain spaces
+    $fullname = "`"" + $fullname + "`""
+    # Specify the command line parameters for wusa.exe
+    $parameters = $fullname + " /quiet /norestart"
+    # Start wusa.exe and pass in the parameters
+    $install = [System.Diagnostics.Process]::Start( "wusa",$parameters )
+    $install.WaitForExit()
+    write-host "Finished installing $msu"
+}
+
+write-host "Restarting Computer"
+#Restart-Computer
